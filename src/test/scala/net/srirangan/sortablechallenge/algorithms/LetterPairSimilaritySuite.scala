42aac146e9805aef3154c2241e238164d96eed77
Srirangan <srirangan@gmail.com>
2012-08-11 13:47:40 +0530

    String similarity algorithm and tests

---
+++ src/test/scala/net/srirangan/sortablechallenge/algorithms/LetterPairSimilaritySuite.scala
@@ -0,0 +1,23 @@
+package net.srirangan.sortablechallenge.algorithms
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+
+class LetterPairSimilaritySuite extends FunSuite with BeforeAndAfter {
+
+  test("'France' and 'Republic of France'") {
+    val score: Double = LetterPairSimilarity.compareStrings("France", "Republic of France")
+    assert(score === 0.5555555555555556)
+  }
+
+  test("'French Republic' and 'Republic of France'") {
+    val score: Double = LetterPairSimilarity.compareStrings("French Republic", "Republic of France")
+    assert(score === 0.8)
+  }
+
+  test("'French Republic' and 'Republic of Cuba'") {
+    val score: Double = LetterPairSimilarity.compareStrings("French Republic", "Republic of Cuba")
+    assert(score === 0.782608695652174)
+  }
+
+}
