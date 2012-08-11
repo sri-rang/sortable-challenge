bd974a6db28a5f3293ae634329cc7e031300e59b
Srirangan <srirangan@gmail.com>
2012-08-11 22:42:16 +0530

    vals not vars ;-)

--- src/test/scala/net/srirangan/sortablechallenge/algorithms/ProductListingMatchSuite.scala
+++ src/test/scala/net/srirangan/sortablechallenge/algorithms/ProductListingMatchSuite.scala
@@ -4,9 +4,9 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 
 class ProductListingMatchSuite extends FunSuite with BeforeAndAfter {
 
-  var rulesPath: String = "/home/srirangan/Projects/sortable-challenge/src/test/resources/rules.json"
-  var productsPath: String = "/home/srirangan/Projects/sortable-challenge/src/test/resources/products.txt"
-  var listingsPath: String = "/home/srirangan/Projects/sortable-challenge/src/test/resources/listings.txt"
+  val rulesPath: String = "/home/srirangan/Projects/sortable-challenge/src/test/resources/rules.json"
+  val productsPath: String = "/home/srirangan/Projects/sortable-challenge/src/test/resources/products.txt"
+  val listingsPath: String = "/home/srirangan/Projects/sortable-challenge/src/test/resources/listings.txt"
 
   test("Match Products and Listings") {
     val matches: List[Map[String, Any]] = ProductListingMatch.execute(rulesPath, productsPath, listingsPath)
