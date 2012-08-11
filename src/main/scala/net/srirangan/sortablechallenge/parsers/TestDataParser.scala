fe5d72de16a51a575286f6dc70adcf3314e1330a
Srirangan <srirangan@gmail.com>
2012-08-11 11:36:43 +0530

    Spec for TestDataParser

---
+++ src/main/scala/net/srirangan/sortablechallenge/parsers/TestDataParser.scala
@@ -0,0 +1,19 @@
+package net.srirangan.sortablechallenge.parsers
+
+object TestDataParser {
+
+  def parse(filePath: String): List[String] = {
+    // Read file per line
+    // Convert line into map
+    // Append map to result list
+    // return
+    null
+  }
+
+  private def parseObject(string: String): Map[String, String] = {
+    // JSON object to map
+    // return
+    null
+  }
+
+}
