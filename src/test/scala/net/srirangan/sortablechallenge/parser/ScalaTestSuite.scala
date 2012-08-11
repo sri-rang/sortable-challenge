73c3d6042ea0eccc52a873c421710b7faaeeffb6
Srirangan <srirangan@gmail.com>
2012-08-11 11:46:12 +0530

    ScalaTest (FunSuite) works!

---
+++ src/test/scala/net/srirangan/sortablechallenge/parser/ScalaTestSuite.scala
@@ -0,0 +1,11 @@
+package net.srirangan.sortablechallenge.parser
+
+import org.scalatest.FunSuite
+
+class ScalaTestSuite extends FunSuite {
+
+  test("Verify ScalaTest works") {
+    assert(1 == 1)
+  }
+
+}
